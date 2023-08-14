6bb1c6443d978049cfd1a3c1b1870a8528a5c3b0
xieguanhao <xieguanhao@qianxin.com>
2023-08-14 15:18:17 +0800

    add

--- yolov8魔改/ODConv+ConvNeXt/ODConv.py
+++ yolov8魔改/ODConv+ConvNeXt/ODConv.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn
+import torch.nn as nn
 # ------------------------------------ODConv start -------------------------------------
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
